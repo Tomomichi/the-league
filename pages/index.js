3d8fe2c654d1c1d409804e0d3ca035168a237b5b
notsobad <tomomichi.onishi@gmail.com>
2020-10-08 16:10:33 +0900

    fix td

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,17 @@ import { firebase } from '../lib/firebase.js'
 import Link from 'next/link'
 
 export default function Index({league}) {
-  const svgString = `<svg xmlns="http://www.w3.org/2000/svg" style="width:100%;height:100%;">
-                      <line x1="0%" y1="0%" x2="100%" y2="100%" style="stroke: #ddd; stroke-width: 2;"/>
-                     </svg>`;
-  const buff = new Buffer(svgString);
-  const svg = buff.toString('base64');
-
   return (
     <div>
       <h1 className="text-lg mb-8">{league.title}</h1>
-      <div style={{ backgroundImage: `url(\'data:image/svg+xml;base64,${svg}\');` }}>
-        <table className="table-fixed w-full border-collapse border" style={{position: 'relative', zIndex: '-1'}}>
+      <div>
+        <table className="table-fixed w-full border-collapse border">
           <thead>
             <tr className="bg-gray-100">
               <th className="border">
               </th>
               { league.teams.map(team => (
-                <th key={team.name} className="border py-3">{team.name}</th>
+                <th key={team.name} className="border py-2 text-sm">{team.name}</th>
               )) }
             </tr>
           </thead>
@@ -27,9 +21,7 @@ export default function Index({league}) {
               <tr key={teamIndex}>
                 <th className="bg-gray-100 border p-3 text-left">{team.name}</th>
                 { league.teams.map((counter, counterIndex) => (
-                  <td key={`${teamIndex}-${counterIndex}`} className="border p-3 text-center">
-                    <Result teamIndex={teamIndex} counterIndex={counterIndex} />
-                  </td>
+                  <Result key={`${teamIndex}-${counterIndex}`} league={league} teamIndex={teamIndex} counterIndex={counterIndex} />
                 )) }
               </tr>
             )) }
@@ -41,9 +33,27 @@ export default function Index({league}) {
 }
 
 const Result = (props) => {
-  console.log("******")
-  console.log(props)
-  return <div>aa</div>
+  const svgString = `<svg xmlns="http://www.w3.org/2000/svg" style="width:100%;height:100%;">
+                      <line x1="0%" y1="0%" x2="100%" y2="100%" style="stroke: #e2e8f0; stroke-width: 1;"/>
+                     </svg>`;
+  const buff = new Buffer(svgString);
+  const svg = buff.toString('base64');
+
+  if(props.teamIndex == props.counterIndex || props.teamIndex > props.counterIndex) {
+    return (
+      <td className="border p-3 text-center" style={{ backgroundImage: `url(\'data:image/svg+xml;base64,${svg}\');` }}>
+        <div></div>
+      </td>
+    );
+  }
+
+  return (
+    <td className="border p-3 text-center cursor-pointer hover:bg-gray-200">
+      <div>
+        { props.league.results[props.teamIndex][0].scores.join("-") }
+      </div>
+    </td>
+  );
 }
 
 
@@ -51,11 +61,6 @@ export async function getStaticProps(context) {
   const docId = "svVUNf8p6vwfpP9V96XW";
   const doc = await firebase.firestore().collection('leagues').doc(docId).get();
   const league = Object.assign(doc.data(), {id: doc.id});
-
-  console.log("-----")
-  console.log(league.results[0]);
-  console.log("=====")
-  console.log(league.results[0][0]);
 
   const defaultResult = {
     0: [
