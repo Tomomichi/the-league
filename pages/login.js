e4af8a60b91083220a987a3108d71cf064eac841
notsobad <tomomichi.onishi@gmail.com>
2020-11-13 17:33:00 +0900

    revert login redirect

--- pages/login.js
+++ pages/login.js
@@ -16,11 +16,13 @@ export default function Login() {
   }, [user]);
 
   // リダイレクト後の処理
-  firebase.auth().getRedirectResult().then(res => {
-    setUser(res.user);
-    openSnackbar('ログインしました');
-    router.push(`/mypage`);
-  }).catch(error => {
+  firebase.auth().getRedirectResult()
+  // .then(res => {
+  //   setUser(res.user);
+  //   openSnackbar('ログインしました');
+  //   router.push(`/mypage`);
+  // })
+  .catch(error => {
     // ゲストから登録済みのユーザーに紐付けようとしたとき
     if(error.code == 'auth/credential-already-in-use') {
       const alertMessage = 'すでに登録済みのアカウントに、ゲストユーザーのデータを引き継ぐことはできません。ログインすると現在ゲストで作成したデータは失われますが、よろしいですか？'
