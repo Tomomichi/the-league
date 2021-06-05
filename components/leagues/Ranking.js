c9ed2d3df5c3a7e5b18f1fce1e86aa996a2b2487
notsobad <tomomichi.onishi@gmail.com>
2021-06-05 12:29:20 +0900

    customize ranking columns

--- components/leagues/Ranking.js
+++ components/leagues/Ranking.js
@@ -6,7 +6,19 @@ const CellWidth = 100;
 export default function Ranking(){
   const [league, setLeague] = useContext(LeagueContext);
 
-  const pointsTable = league.pointsTable || { win: 3, lose: 0, draw: 1 }
+  // 勝ち点計算ロジック
+  const pointsTable = league.pointsTable || { win: 3, lose: 0, draw: 1 };
+
+  // ランキング表の表示項目・表示順
+  const rankingColumns = league.rankingColumns || [
+    { point: '勝ち点' },
+    { win: '勝ち' },
+    { draw: '引き分け' },
+    { lose: '負け' },
+    { gf: '得点' },
+    { ga: '失点' },
+    { gd: '得失差' },
+  ];
 
   const calcPoints = () => {
     let p = {};
@@ -75,13 +87,10 @@ export default function Ranking(){
             <tr>
               <th className="px-2 py-3 text-left" style={{width: 70}}>順位</th>
               <th className="px-2 py-3 text-left" style={{width: 200}}>名前</th>
-              <th className="px-2 py-3 text-left" style={{width: 70}}>勝ち点</th>
-              <th className="px-2 py-3 text-left" style={{width: 70}}>勝ち</th>
-              <th className="px-2 py-3 text-left" style={{width: 70}}>引き分け</th>
-              <th className="px-2 py-3 text-left" style={{width: 70}}>負け</th>
-              <th className="px-2 py-3 text-left" style={{width: 70}}>得点</th>
-              <th className="px-2 py-3 text-left" style={{width: 70}}>失点</th>
-              <th className="px-2 py-3 text-left" style={{width: 70}}>得失差</th>
+
+              { rankingColumns.map(column => (
+                <th key={Object.keys(column)[0]} className="px-2 py-3 text-left" style={{width: 70}}>{ Object.values(column)[0] }</th>
+              ))}
             </tr>
           </thead>
           <tbody>
@@ -89,24 +98,14 @@ export default function Ranking(){
               <tr key={tid} className="border-b hover:bg-gray-100">
                 <td className="px-2 py-3">{index + 1}</td>
                 <td className="px-2 py-3">{ league.teams.find(t => t.id == tid).name }</td>
-                <td className="px-2 py-3">{points[tid].point}</td>
-                <td className="px-2 py-3">{points[tid].win}</td>
-                <td className="px-2 py-3">{points[tid].draw}</td>
-                <td className="px-2 py-3">{points[tid].lose}</td>
-                <td className="px-2 py-3">{points[tid].gf}</td>
-                <td className="px-2 py-3">{points[tid].ga}</td>
-                <td className="px-2 py-3">{points[tid].gd}</td>
+
+                { rankingColumns.map(column => (
+                  <td key={Object.keys(column)[0]} className="px-2 py-3">{ points[tid][Object.keys(column)[0]] }</td>
+                ))}
               </tr>
             ))}
           </tbody>
         </table>
-      </div>
-
-      <div className="mt-8 text-xs">
-        ※ランキングは「勝点→得失点→得点」順で、勝ち点は勝ちが「3点」,引き分けを「1点」として計算しています。
-        点数のカスタマイズや得失点などの表示も将来的に実装予定です。
-        <br />
-        ※独自要件が必要な場合、カスタム開発での対応も可能です。お問い合わせからお気軽にご相談ください。
       </div>
     </>
   )
